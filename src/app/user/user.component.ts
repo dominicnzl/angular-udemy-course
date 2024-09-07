5893348f5ae15044b2b26e6e126b5b64d12dfc64
dng <dominicnzl+git@gmail.com>
2024-09-07 08:39:35 +0200

    angular-udemy-course * created user component with dummy users and signals to maintain state

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import {Component, computed, signal} from '@angular/core';
+import {DUMMY_USERS} from "../dummy-users";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
+  // dng: [07/09/2024] this would be one way to provide an imagePath when not using signals
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+}
