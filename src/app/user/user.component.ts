568f75fa1d0e3b9b91be2f5127b13eb0137b5ae4
dng <dominicnzl+git@gmail.com>
2025-03-03 19:15:56 +0100

    stopped after chapter 44: https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/43788608#overview

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import {Component, computed, Input, signal} from '@angular/core';
+import {Component, computed, EventEmitter, Input, Output, signal} from '@angular/core';
 import {DUMMY_USERS} from "../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {User} from "../model/User";
 
 @Component({
   selector: 'app-user',
@@ -11,13 +10,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
+    this.select.emit(this.user.id);
   }
 }
